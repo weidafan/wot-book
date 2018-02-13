166865142e7e2fea4698a657f1ab838b3e4d8c25
WoT_House <philip.pinola.14@cnu.edu>
2018-02-13 20:54:49 +0000

    add sparkfun moisture sensor

---
+++ moisture_sensor/soil.js
@@ -0,0 +1,15 @@
+var Gpio = require('onoff').Gpio;
+  led = new Gpio(27, 'out');
+var Mcp3008 = require('mcp3008.js');
+    adc = new Mcp3008();
+    channel = 1;
+adc.poll(channel,1003, function (value) {
+    console.log("The soil humidity is: "+value);
+    if(value>500){
+	led.writeSync(1);
+    }
+    else{
+	led.writeSync(0);
+    }	
+});
+
